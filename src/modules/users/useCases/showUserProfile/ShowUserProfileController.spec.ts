196ba6b88c2e57d14390ededb636d7de249cae5a
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-03-04 12:13:14 -0300

    :hammer: feat: Create test for ShowUserProfileController

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -0,0 +1,44 @@
+import request from "supertest";
+import { app } from "../../../../app";
+
+import { DataSource } from "typeorm";
+import { createConnection } from "../../../../database";
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
+
+let connection: DataSource;
+
+const user: ICreateUserDTO = {
+  name: "John",
+  password: "12345",
+  email: "james@example.com",
+};
+
+describe("ShowUserProfileController", () => {
+  beforeAll(async () => {
+    connection = await createConnection("localhost");
+
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.destroy();
+  });
+
+  it("should be able show user profile", async () => {
+    await request(app).post("/api/v1/users").send(user);
+
+    const responseToken = await request(app).post("/api/v1/sessions").send({
+      email: user.email,
+      password: user.password,
+    });
+
+    const token = responseToken.body.token;
+
+    const response = await request(app)
+      .get("/api/v1/profile")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.body).toHaveProperty("id");
+  });
+});
